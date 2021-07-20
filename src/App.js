75af40eb791b7c08c579147e17f0d3f6c8aa792c
albertvr357 <cuervo357@gmail.com>
2021-07-19 23:24:59 -0500

    header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Social from './components/Social/Social';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <div className='left_container'>
         <Social />
+        <Header />
       </div>
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Header.module.css';
+
+function Header() {
+  return (
+    <div className={style.container}>
+      Header component
+    </div>
+  )
+}
+
+export default Header;
