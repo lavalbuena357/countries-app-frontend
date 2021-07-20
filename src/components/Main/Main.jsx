ded7da9fb1ed12cf8174599204aa3ec368e10b77
albertvr357 <cuervo357@gmail.com>
2021-07-20 01:56:51 -0500

    pagination component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Main.module.css';
+import Pagination from './Pagination/Pagination';
 
 function Main() {
   return (
     <div className={style.container}>
-      Main component
+      <Pagination />
     </div>
   )
 }
---
+++ src/components/Main/Pagination/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Pagination.module.css';
+
+function Pagination() {
+  return (
+    <div className={style.container}>
+      Pagination component
+    </div>
+  )
+}
+
+export default Pagination;
