e6eef8c66a2e77098a981c2165d72f508877e101
albertvr357 <cuervo357@gmail.com>
2021-07-20 18:46:01 -0500

    social icons

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import style from './Social.module.css';
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
 
 function Social() {
   return (
     <div className={style.container}>
-      Social component
+      <FaGithub />
+      <FaLinkedin />
+      <FaInstagram />
+      <FaFacebook />
+      <FaTwitter />
     </div>
   )
 }
