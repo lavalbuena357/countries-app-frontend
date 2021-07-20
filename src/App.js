599d8aa5ddb16a136fd23622bdf2b1d33291b647
albertvr357 <cuervo357@gmail.com>
2021-07-19 23:26:51 -0500

    filters component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Social from './components/Social/Social';
 import Header from './components/Header/Header';
+import Filters from './components/Filters/Filters';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <div className='left_container'>
         <Social />
         <Header />
+      </div>
+      <div className='right_container'>
+        <Filters />
       </div>
     </div>
   );
---
+++ src/components/Filters/Filters.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Filters.module.css';
+
+function Filters() {
+  return (
+    <div className={style.container}>
+      Filters component
+    </div>
+  )
+}
+
+export default Filters;
