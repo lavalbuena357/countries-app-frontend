e9d24ba5a25c76b5d9dfcb5dcc898e5073f54666
albertvr357 <cuervo357@gmail.com>
2021-07-20 18:58:06 -0500

    header module

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import style from './Header.module.css';
+import { Link } from 'react-router-dom';
+import logo from '../../utils/logo.png';
 
 function Header() {
   return (
     <div className={style.container}>
-      Header component
+      <Link to='/'>
+        <img src={logo} alt="logo countries app" />
+      </Link>
+      <div className={style.text}>
+        <h1>Find country information</h1>
+      </div>
     </div>
   )
 }
