1dfef00e46560c2ac1956336cbd7d6102638c9d4
albertvr357 <cuervo357@gmail.com>
2021-07-20 21:52:53 -0500

    country detail component

---
+++ src/components/CountryDetail/CountryDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './CountryDetail.module.css';
+
+function CountryDetail() {
+  return (
+    <div className={style.container}>
+      
+    </div>
+  )
+}
+
+export default CountryDetail;
