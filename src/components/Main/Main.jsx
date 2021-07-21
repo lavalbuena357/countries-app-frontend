4fc96c2a52b4981ca9df41dedb8997dc572a0a4f
albertvr357 <cuervo357@gmail.com>
2021-07-20 21:53:18 -0500

    get countries

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getCountries } from '../../redux/actions';
 import style from './Main.module.css';
 import Pagination from './Pagination/Pagination';
 import Cards from './Cards/Cards';
 
-function Main() {
+function Main({ getCountries, countries }) {
+
+  useEffect(() => {
+    getCountries()
+  }, []) // eslint-disable-line
+
   return (
     <div className={style.container}>
       <Pagination />
-      <Cards />
+      <Cards countries={countries}/>
       <Pagination />
     </div>
   )
 }
 
-export default Main;
+function mapStateToProps(state) {
+  return {
+    countries: state.countries
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getCountries: () => dispatch(getCountries())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
