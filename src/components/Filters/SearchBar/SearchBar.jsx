9450daa3581ba822cbb86d77a3ca6fc447e9b846
albertvr357 <cuervo357@gmail.com>
2021-07-21 11:54:17 -0500

    searchbar

--- src/components/Filters/SearchBar/SearchBar.jsx
+++ src/components/Filters/SearchBar/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import style from './SearchBar.module.css';
 import { connect } from 'react-redux';
 import { searchCountries } from '../../../redux/actions';
+import { FaSearch } from 'react-icons/fa';
 
 function SearchBar({ searchCountries }) {
   const [name, setName] = useState('')
@@ -13,9 +14,10 @@ function SearchBar({ searchCountries }) {
 
   return (
     <div className={style.container}>
+      <FaSearch className={style.icon} />
       <input
         className={style.search_bar}
-        type = 'text'
+        type = 'search'
         value = {name}
         placeholder = 'Search Countries...'
         onChange={(e) => handleChange(e)}
