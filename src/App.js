96b844c5ffa4458efd05fc1fa5394ed093337307
albertvr357 <cuervo357@gmail.com>
2021-07-20 21:50:00 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
+import { Route } from 'react-router-dom';
 import Social from './components/Social/Social';
 import Header from './components/Header/Header';
 import Filters from './components/Filters/Filters';
 import Main from './components/Main/Main';
+import CountryDetail from './components/CountryDetail/CountryDetail';
 
 function App() {
   return (
     <div className="App">
       <div className='left_container'>
-        <Social />
-        <Header />
+        <Route path='/' component={Social} />
+        <Route path='/' component={Header} />
       </div>
       <div className='right_container'>
-        <Filters />
-        <Main />
+        <Route exact path='/' component={Filters} />
+        <Route exact path='/' component={Main} />
       </div>
+      <Route exact path='detail/:id' component={CountryDetail} />
     </div>
   );
 }
