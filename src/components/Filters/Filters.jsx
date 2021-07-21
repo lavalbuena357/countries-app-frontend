63d2bd65171949aedc715909ae9e81633e1ca1d7
albertvr357 <cuervo357@gmail.com>
2021-07-20 19:34:40 -0500

    search bar

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './Filters.module.css';
+import SearchBar from './SearchBar/SearchBar';
 
 function Filters() {
   return (
     <div className={style.container}>
-      Filters component
+      <SearchBar />
     </div>
   )
 }
---
+++ src/components/Filters/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './SearchBar.module.css';
+
+function SearchBar() {
+  return (
+    <div className={style.container}>
+      <div>
+        <input type="text" name="" id="" />
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar;
