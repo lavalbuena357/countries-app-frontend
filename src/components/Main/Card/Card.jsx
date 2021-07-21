b574ed33e81dce7908942746d311f8c3d0cc52ec
albertvr357 <cuervo357@gmail.com>
2021-07-20 21:54:05 -0500

    card component

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './Card.module.css';
 
-function Card() {
+function Card({ name, flag, continent, id, population }) {
   return (
     <div className={style.container}>
-      Card component
+      <Link to={`detail/${id}`}>
+        <div className={style.flag}>
+          <img src={flag} alt="country flag" />
+        </div>
+        <h1>{name}</h1>
+        <h3>{continent}</h3>
+        <p>Population: {population}</p>
+      </Link>
     </div>
   )
 }
