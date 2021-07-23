d00d7f242d495d805a3ded6ca633a854d0729b56
albertvr357 <cuervo357@gmail.com>
2021-07-22 20:54:39 -0500

    action creator country detail

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 export const GET_COUNTRIES = 'GET_COUNTRIES';
 export const SEARCH_COUNTRIES = 'SEARCH_COUNTRIES';
 export const FILTER_CONTINENT = 'FILTER_CONTINENT';
+export const COUNTRY_DETAIL = "COUNTRY_DETAIL";
 
 export function getCountries() {
   return (
@@ -41,6 +42,18 @@ export function filterContinent(continent) {
       dispatch({
         type: FILTER_CONTINENT,
         payload: continents.data
+      })
+    } catch(err) {console.log(err)}
+  }
+}
+
+export function countryDetail(id) {
+  return async function(dispatch) {
+    try {
+      const country = await axios.get(`https://countriesinfoapp.herokuapp.com/countries/${id}`);
+      dispatch({
+        type: COUNTRY_DETAIL,
+        payload: country.data
       })
     } catch(err) {console.log(err)}
   }
