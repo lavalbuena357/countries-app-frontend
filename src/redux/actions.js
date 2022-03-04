f1f0643bcb4369fa82ef751864da6a27f65bd802
lavalbuena357 <cuervo357@gmail.com>
2022-03-04 14:19:53 -0500

    accion filtrar paises por moneda

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const url = process.env.REACT_APP_URI
 
+//traer todos los paises
 export function getCountries() {
   return async function(dispatch) {
     try {
@@ -14,6 +15,7 @@ export function getCountries() {
   }
 }
 
+//buscar pais por codigo
 export function getCountryByCode(code) {
   return async function(dispatch) {
     try {
@@ -21,6 +23,19 @@ export function getCountryByCode(code) {
       dispatch({
         type: "COUNTRY_BY_CODE",
         payload: country.data
+      })
+    } catch (error) {console.log(error)}
+  }
+}
+
+//filtrar paises por moneda
+export function filterCountriesByCurrency(currency) {
+  return async function(dispatch) {
+    try {
+      const countriesCurrency = await axios.get(`${url}/currency/${currency}`)
+      dispatch({
+        type: "COUNTRIES_BY_CURRENCY",
+        payload: countriesCurrency.data
       })
     } catch (error) {console.log(error)}
   }
