5ebd5c947f1c7311330b686322ef05307eb5d475
lavalbuena357 <cuervo357@gmail.com>
2022-03-04 14:14:13 -0500

    buscar ciudad por codigo

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,10 +4,24 @@ const url = process.env.REACT_APP_URI
 
 export function getCountries() {
   return async function(dispatch) {
-    const countries = await axios.get(`${url}/all`)
-    dispatch({
-      type: "COUNTRIES",
-      payload: countries.data
-    })
+    try {
+      const countries = await axios.get(`${url}/all`)
+      dispatch({
+        type: "COUNTRIES",
+        payload: countries.data
+      })
+    } catch (error) { console.log(error)}
+  }
+}
+
+export function getCountryByCode(code) {
+  return async function(dispatch) {
+    try {
+      const country = await axios.get(`${url}/alpha/${code}`)
+      dispatch({
+        type: "COUNTRY_BY_CODE",
+        payload: country.data
+      })
+    } catch (error) {console.log(error)}
   }
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,14 @@
 const initialState = {
-  countries: []
+  countries: [],
+  currentCountry: null
 }
 
 export default function reducer(state=initialState, action) {
   switch(action.type) {
     case "COUNTRIES": 
       return {...state, countries: action.payload}
+    case "COUNTRY_BY_CODE":
+      return {...state, currentCountry: action.payload}
     default:
       return state
   }
