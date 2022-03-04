15fe880fe4d62b0887d616472195be7c55ef8037
lavalbuena357 <cuervo357@gmail.com>
2022-03-04 14:10:33 -0500

    accion obtener ciudades

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+const url = process.env.REACT_APP_URI
+
 export function getCountries() {
-  
+  return async function(dispatch) {
+    const countries = await axios.get(`${url}/all`)
+    dispatch({
+      type: "COUNTRIES",
+      payload: countries.data
+    })
+  }
 }
