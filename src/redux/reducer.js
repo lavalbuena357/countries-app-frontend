98695b1804f7350a3a3a92dcb638ec5c03dbb179
lavalbuena357 <cuervo357@gmail.com>
2022-03-09 14:33:32 -0500

    reducer - cargar todos los tipos de moneda al inicio de la app

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,14 @@
 const initialState = {
   countries: [],
-  currentCountry: null
+  currentCountry: null,
+  currencies: []
 }
 
 export default function reducer(state=initialState, action) {
   switch(action.type) {
     case "COUNTRIES": 
+      return {...state, countries: action.payload}
+    case "COUNTRY_BY_NAME":
       return {...state, countries: action.payload}
     case "COUNTRY_BY_CODE":
       return {...state, currentCountry: action.payload}
@@ -15,6 +18,22 @@ export default function reducer(state=initialState, action) {
       return {...state, countries: action.payload}
     case "COUNTRIES_BY_CONTINENT":
       return {...state, countries: action.payload}
+    case "SET_CURRENCIES":
+      const allCurrencies = state.countries.map(el => el.currencies)
+      const multipleObjects = allCurrencies.filter(el => el && Object.keys(el).length > 1)
+      const singleObjects = allCurrencies.filter(el => el && Object.keys(el).length === 1)
+      multipleObjects.forEach(el => {
+        for (let item in el) {
+          singleObjects[singleObjects.length] = {[item]:el[item]}
+        }
+      })
+      let hash = {}
+      const filterCurrencies = singleObjects.filter(el => {
+        for (let item in el) {
+          return hash[item] ? false : hash[item] = true
+        }
+      })
+      return {...state, currencies: filterCurrencies}
     default:
       return state
   }
