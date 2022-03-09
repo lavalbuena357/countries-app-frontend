4980d0482cd35cc53908fb3fba33e1c5236a206c
lavalbuena357 <cuervo357@gmail.com>
2022-03-09 14:37:15 -0500

    carga los estados iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import Header from './components/Header/Header'
+import Nav from './components/Nav/Nav'
+import { getCountries, setCurrencies } from './redux/actions'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const countries = useSelector(state => state.countries)
+
+  useEffect(async() => { // eslint-disable-line
+    await dispatch(getCountries()) 
+    if(countries.length === 0) {
+      await dispatch(setCurrencies())
+    }
+  }, [])
+
+  
+
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Nav />
+    </div>
   )
 }
 
