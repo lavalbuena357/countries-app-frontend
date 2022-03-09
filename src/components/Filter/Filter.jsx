d43703c100f33a6acf2a5470c87e67b52326edcf
lavalbuena357 <cuervo357@gmail.com>
2022-03-09 15:18:27 -0500

    componente filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
-function Filter() {
+function Filter(props) {
+
+  const dispatch = useDispatch()
+
+  function handleChange(e) {
+    dispatch(props.action(e.target.value))
+  }
+
   return (
     <div>
-      Filters
+      <select onChange={handleChange}>
+        <option value="">{props.text}</option>
+        {
+          props.mapArr.map((el, i) => (
+            <option key={i} value={props.valuePred(el)}>{props.optionsText(el)}</option>
+          ))
+        }
+      </select>
     </div>
   )
 }
