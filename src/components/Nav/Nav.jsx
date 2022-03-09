d153345f704979f2287fa03494226adce011e1da
lavalbuena357 <cuervo357@gmail.com>
2022-03-09 15:18:50 -0500

    añadiendo componentes filters al nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { filterCountriesByCurrency } from '../../redux/actions'
 import Filter from '../Filter/Filter'
 
 function Nav() {
+
+  const currencies = useSelector(state => state.currencies)
+
   return (
     <div>
-      <Filter />
+      <Filter 
+        action={filterCountriesByCurrency}
+        text='Filtrar por moneda' 
+        mapArr={currencies} 
+        valuePred={el => Object.keys(el)[0]}
+        optionsText={el => `${Object.keys(el)[0]} - ${Object.values(el)[0].name} - ${Object.values(el)[0].symbol}`}
+      />
     </div>
   )
 }
