0eeecfc6cfdcc0dc909b4798140aadd753880d3a
lavalbuena357 <cuervo357@gmail.com>
2022-03-09 16:18:55 -0500

    componente filtro por continentes añadido

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { filterCountriesByCurrency } from '../../redux/actions'
+import { filterCountriesByContinent, filterCountriesByCurrency, filterCountriesByLanguage } from '../../redux/actions'
 import Filter from '../Filter/Filter'
 
 function Nav() {
 
   const currencies = useSelector(state => state.currencies)
+  const languages = useSelector(state => state.languages)
+  const continents = useSelector(state => state.continents)
 
   return (
     <div>
@@ -15,6 +17,22 @@ function Nav() {
         mapArr={currencies} 
         valuePred={el => Object.keys(el)[0]}
         optionsText={el => `${Object.keys(el)[0]} - ${Object.values(el)[0].name} - ${Object.values(el)[0].symbol}`}
+      />
+
+      <Filter 
+        action={filterCountriesByLanguage}
+        text='Filtrar por idioma' 
+        mapArr={languages} 
+        valuePred={el => Object.keys(el)[0]}
+        optionsText={el => Object.values(el)[0]}
+      />
+
+      <Filter 
+        action={filterCountriesByContinent}
+        text='Filtrar por continente' 
+        mapArr={continents} 
+        valuePred={el => Object.keys(el)[0]}
+        optionsText={el => Object.values(el)[0]}
       />
     </div>
   )
