80658a984f81f013734777e1b60c4b40eab8fdf9
lavalbuena357 <cuervo357@gmail.com>
2022-03-16 12:37:40 -0500

    loader de prueba

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Main from './pages/Main/Main'
-import { getCountries, setCurrencies, setLanguages } from './redux/actions'
+import { getCountries, loader, setCurrencies, setLanguages } from './redux/actions'
 
 function App() {
 
@@ -11,11 +11,13 @@ function App() {
   const countries = useSelector(state => state.countries)
 
   useEffect(async() => { // eslint-disable-line
+    dispatch(loader(true))
     await dispatch(getCountries()) 
     if(!countries.countries) { 
       await dispatch(setCurrencies())
       await dispatch(setLanguages())
     }
+    dispatch(loader(false))
   }, [])  
 
   return (
