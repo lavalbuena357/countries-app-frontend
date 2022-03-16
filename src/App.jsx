d350f0fff60ae98b33fd1119ca2a0792a4d14f06
lavalbuena357 <cuervo357@gmail.com>
2022-03-16 10:22:10 -0500

    cargado inicial de listado de monedas y lenguajes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
-import { getCountries, setCurrencies } from './redux/actions'
+import { getCountries, setCurrencies, setLanguages } from './redux/actions'
 
 function App() {
 
@@ -11,12 +11,11 @@ function App() {
 
   useEffect(async() => { // eslint-disable-line
     await dispatch(getCountries()) 
-    if(countries.length === 0) {
+    if(!countries.countries) { 
       await dispatch(setCurrencies())
+      await dispatch(setLanguages())
     }
-  }, [])
-
-  
+  }, [])  
 
   return (
     <div>
