809629028dae1bdace1574c81b670b2ed75efc05
lavalbuena357 <cuervo357@gmail.com>
2022-03-16 10:17:21 -0500

    componente barra de busqueda

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import SearchBar from '../SearchBar/SearchBar'
 
 function Header() {
   return (
     <div>
       <h1>Conociendo el mundo</h1>
+      <SearchBar />
     </div>
   )
 }
