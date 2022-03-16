07973eeb644aa6fda774ca185163ef6462a8c8a6
lavalbuena357 <cuervo357@gmail.com>
2022-03-16 10:35:25 -0500

    cambio de ubicacion de barra de busqueda

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import SearchBar from '../SearchBar/SearchBar'
 
 function Header() {
   return (
     <div>
       <h1>Conociendo el mundo</h1>
-      <SearchBar />
     </div>
   )
 }
