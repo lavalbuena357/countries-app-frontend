c843857d2fc543f2c694b907d877f3048de2e053
lavalbuena357 <cuervo357@gmail.com>
2022-03-16 12:37:24 -0500

    se implementaron clases y renderizado condicional para el escudo de cada pais

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './Card.module.css'
 
 function Card(props) {
   return (
-    <div>
-      <div>
-        <img src={props.coat} alt="coat" />
-        <img src={props.flag} alt="coat" />
+    <div className={styles.container}>
+      <div className={styles.images}>
+        {props.coat.length > 0 && <img className={styles.coat} src={props.coat} alt="coat" />}
+        <img className={styles.flag} src={props.flag} alt="coat" />
       </div>
-      <div>
+      <div className={styles.info}>
         <h3>{props.code}</h3>
         <h2>{props.name}</h2>
         <h4>{props.continent}</h4>
