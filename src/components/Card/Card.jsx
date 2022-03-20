c6176af2048b000e8d5a9ac00ff30d47be322e03
lavalbuena357 <cuervo357@gmail.com>
2022-03-20 14:28:52 -0500

    funcion para ver el detalle de cada pais

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getCountryById } from '../../redux/actions'
 import styles from './Card.module.css'
 
 function Card(props) {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  async function goToDetail(id) {
+    await dispatch(getCountryById(id))
+    navigate(`/${id}`)
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => goToDetail(props.code)}>
       <div className={styles.images}>
         {/* {props.coat.length > 0 && <img className={styles.coat} src={props.coat} alt="coat" />} */}
         <img className={styles.flag} src={props.flag} alt="coat" />
