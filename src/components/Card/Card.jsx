1c952ebb0a3a5aca1ea9d1acf18f4f6efa4a451e
lavalbuena357 <cuervo357@gmail.com>
2022-03-20 13:26:36 -0500

    card estilos y se quito escudo por temas de rendimiento

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,17 +5,17 @@ function Card(props) {
   return (
     <div className={styles.container}>
       <div className={styles.images}>
-        {props.coat.length > 0 && <img className={styles.coat} src={props.coat} alt="coat" />}
+        {/* {props.coat.length > 0 && <img className={styles.coat} src={props.coat} alt="coat" />} */}
         <img className={styles.flag} src={props.flag} alt="coat" />
       </div>
       <div className={styles.info}>
         <h3>{props.code}</h3>
         <h2>{props.name}</h2>
         <h4>{props.continent}</h4>
-        <div>
+        <div className={styles.language_ctn}>
           {
             props.lang.map((el, i) => (
-              <span key={i}>{el.name}</span>
+              <span className={styles.language_item} key={i}>{el.name}</span>
             ))
           }
         </div>
