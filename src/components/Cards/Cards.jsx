a54c3c85fb3c07e7309569187c32abff4561e552
lavalbuena357 <cuervo357@gmail.com>
2022-03-20 13:26:05 -0500

    cards estilos , filtrado correcto y correccion de bug al cargar mas

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Card from '../Card/Card'
+import styles from './Cards.module.css'
 
 function Cards() {
+  const [blocks, setBlocks] = useState(1)
 
   const { countries , loader, searchCountries } = useSelector(state => state)
 
+  useEffect(() => {
+    setBlocks(1)
+  }, [countries])
+
+  function more() {
+    setBlocks(prev => prev+1)
+  }
+
   return (
-    <div style={{display:'flex', flexWrap:'wrap'}}>
+    <div className={styles.container}>
       {
         loader ? <p>cargando...</p> : 
         (searchCountries.countries && searchCountries.countries.length === 0) || countries.countries && countries.countries.length === 0 ? <span>No se encontraron resultados...</span> :
         searchCountries.countries ? 
-        searchCountries.countries.map((el, i) => (
-          <Card key={i} coat={el.coats_of_arms} flag={el.flag} code={el.id} name={el.name} continent={el.continent} lang={el.languages} />
-        )) :
-        countries.countries && countries.countries.map((el, i) => (
-          <Card key={i} coat={el.coats_of_arms} flag={el.flag} code={el.id} name={el.name} continent={el.continent} lang={el.languages} />
-        ))
+        <div className={styles.cards_box}>
+          <p>{searchCountries.countries.length} {searchCountries.countries.length > 1 ? 'paises' : 'país'} encontrados</p>
+          <div className={styles.cards_ctn}>
+            {searchCountries.countries.slice(0, blocks*20).map((el, i) => (
+            <Card key={i} coat={el.coats_of_arms} flag={el.flag} code={el.id} name={el.name} continent={el.continent} lang={el.languages} />
+            ))}
+          </div>
+          <button onClick={more}>{(searchCountries.countries.length - (blocks*20) >= 0) ? `${searchCountries.countries.length - (blocks*20)} más...` : `0 Restantes` }</button>
+        </div> 
+        :
+        <div className={styles.cards_box}>
+          <p>{countries.countries.length} paises encontrados</p>
+          <div className={styles.cards_ctn}>
+            {countries.countries.slice(0, blocks*20).map((el, i) => (
+              <Card key={i} coat={el.coats_of_arms} flag={el.flag} code={el.id} name={el.name} continent={el.continent} lang={el.languages} />
+            ))}
+          </div>
+          <button onClick={more}>{countries.countries && (countries.countries.length - (blocks*20) >= 0) ? `${countries.countries.length - (blocks*20)} más...` : `0 Restantes` }</button>
+        </div>
+        
+        
       }
     </div>
   )
