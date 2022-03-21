80d518c4e53c5200eae4df55714b72e5aef58598
lavalbuena357 <cuervo357@gmail.com>
2022-03-21 12:10:06 -0500

    correcion de ruta con parametros

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
-import { getCountryById } from '../../redux/actions'
 import styles from './Card.module.css'
 
 function Card(props) {
 
   const navigate = useNavigate()
-  const dispatch = useDispatch()
 
   async function goToDetail(id) {
-    await dispatch(getCountryById(id))
     navigate(`/${id}`)
   }
 
